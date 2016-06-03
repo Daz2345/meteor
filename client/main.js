ff595dc551fa59ffe30ffe2498a668338d3ea22b
Daz2345 <darrenimpey@btinternet.com>
2016-06-03 19:56:28 +0100

    First commit of meteor with React boilerplate project starter

---
+++ client/main.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>Hello There!</div>
+  );
+};
+
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector('.rootContainer'));
+});
